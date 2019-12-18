8631ef06b689f0ce571999a6237f65806dc7d675
Maxim <35666350+Plusez@users.noreply.github.com>
2019-12-18 16:55:52 +0500

    Module 5\2 edition 1
    Module 5\2 edition 1

--- module5/2/Todolist/src/Loader.java
+++ module5/2/Todolist/src/Loader.java
@@ -1,64 +1,108 @@
-import org.w3c.dom.stylesheets.LinkStyle;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class Loader {
+
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         ArrayList<String> toDoList = new ArrayList<>();
         System.out.println("Создан новый список дел. Перечень команд для управления: 1.LIST, 2.ADD, 3.ADD N, 4.EDIT N, 5.DELETE N");
+
         for (; ; ) {
-            Scanner sc = new Scanner(System.in);
+            String commandToDo = new String();
+            String numberPos = new String();
+            String nameToDo = new String();
+            System.out.println("commandToDo - " + commandToDo);
+            System.out.println("numberPos - " + numberPos);
+            System.out.println("nameToDo - " + nameToDo);
+
             System.out.print("Введите имя команды:");
-            String commandName = sc.nextLine();
-            int indexOfSpace = commandName.indexOf(" ");
-            if (commandName.equals("LIST")) {                                               //LIST
-                for (int i = 0; i < toDoList.size(); i++) {
-                    System.out.println(i + " - " + toDoList.get(i));
+            String commandName = scanner.nextLine();
+            String[] commandLine = commandName.split(" ");
+
+            System.out.println();
+            if (commandLine.length == 1) {
+                commandToDo = commandLine[0];
+            } else if (commandLine.length == 2) {
+                commandToDo = commandLine[0];
+                numberPos = commandLine[1];
+            } else if (commandLine.length == 3) {
+                commandToDo = commandLine[0];
+                numberPos = commandLine[1];
+                nameToDo = commandLine[2];
+            }
+
+            if (commandToDo.equals("LIST")) {                                                   //LIST
+                if (toDoList.size() == 0) {
+                    System.out.println("Список пуст");
+                } else {
+                    for (int i = 0; i < toDoList.size(); i++) {
+                        System.out.println(i + " - " + toDoList.get(i));
+                    }
                 }
             }
-            if (commandName.equals("ADD")) {                                                //ADD
-                Scanner sc2 = new Scanner(System.in);
-                System.out.println("Введите дело, которое необходимо добавить");
-                String someWorkAdd = sc2.nextLine();
-                toDoList.add(someWorkAdd);
+            if (commandToDo.equals("ADD") && commandLine.length == 1) {                        //ADD
+                System.out.println("Введите дело, которое необходимо добавить(ADD)");
+                nameToDo = scanner.nextLine();
+                toDoList.add(nameToDo);
             }
-            if (indexOfSpace == 3) {                                                        //ADD N
-                Scanner sc3 = new Scanner(System.in);
+            if (commandToDo.equals("ADD") && commandLine.length == 2) {                          //ADD N
                 System.out.println("Введите номер строки для внесения дела в список");
-                int numOfStrAddN = sc3.nextInt();
-                if (toDoList.size() < numOfStrAddN | toDoList.size() == 0) {
+                int numberPosThis = scanner.nextInt();
+                if (toDoList.size() < numberPosThis || toDoList.size() == 0) {
                     System.out.println("Номер указан неверно");
                 } else {
-                    Scanner sc4 = new Scanner(System.in);
-                    System.out.println("Введите название дела для внесения в список");
-                    String someWorkAddN = sc4.nextLine();
-                    toDoList.add(numOfStrAddN, someWorkAddN);
+                    System.out.println("Введите дело, которое необходимо добавить (ADD N)");
+                    nameToDo = scanner.nextLine();
+                    toDoList.add(numberPosThis, nameToDo);
                 }
             }
-            if (indexOfSpace == 4) {                                                        //EDIT N
-                Scanner sc5 = new Scanner(System.in);
-                System.out.println("Введите номер строки для редактирования дела");
-                int numOfStrEditN = sc5.nextInt();
-                if (toDoList.size() < numOfStrEditN | toDoList.size() == 0) {
+            if (commandToDo.equals("ADD") && commandLine.length == 3) {                               //ADD n text
+                if (toDoList.size() < Integer.parseInt(numberPos) || toDoList.size() == 0) {
                     System.out.println("Номер указан неверно");
                 } else {
-                    Scanner sc6 = new Scanner(System.in);
-                    System.out.printf("Какое дело помещаем в строку %d?", numOfStrEditN);
-                    String someWorkEditN = sc6.nextLine();
-                    toDoList.add(numOfStrEditN, someWorkEditN);
-                    toDoList.remove(numOfStrEditN + 1);
+                    int numberPosThis = Integer.parseInt(numberPos);
+                    toDoList.add(numberPosThis, nameToDo);
                 }
-                if (indexOfSpace == 6) {                                                       //DELETE N
-                    Scanner sc7 = new Scanner(System.in);
-                    System.out.println("Введите номер строки для удаления дела");
-                    int numOfStrDel = sc7.nextInt();
-                    if (toDoList.size() < numOfStrDel | toDoList.size() == 0) {
+            }
+            if (commandToDo.equals("EDIT")) {
+                {
+                    if (commandLine.length == 2) {                                                        //EDIT N 2 варианта - "EDIT N" и "EDIT n text"
+                        System.out.println("Введите номер строки для редактирования дела");
+                        int numberPosThis = scanner.nextInt();
+                        if (toDoList.size() < numberPosThis || toDoList.size() == 0) {
+                            System.out.println("Номер указан неверно");
+                        } else {
+                            System.out.printf("Какое дело помещаем в строку %d?", numberPosThis);
+                            nameToDo = scanner.nextLine();
+                            toDoList.add(numberPosThis, nameToDo);
+                            toDoList.remove(numberPosThis + 1);
+                        }
+                    }
+                }
+                if (commandLine.length == 3) {
+                    System.out.println("Введите номер строки для редактирования дела");
+                    int numberPosThis = scanner.nextInt();
+                    if (toDoList.size() < numberPosThis || toDoList.size() == 0) {
                         System.out.println("Номер указан неверно");
                     } else {
-                        toDoList.remove(numOfStrDel);
+                        toDoList.add(numberPosThis, nameToDo);
+                        toDoList.remove(numberPosThis + 1);
+                    }
+                }
+            }
+            if (commandToDo.equals("DELETE")) {
+                if (numberPos.isEmpty()) {                                                       //DELETE N
+                    System.out.println("Введите номер строки для удаления дела");
+                    int numberPosThis = scanner.nextInt();
+                    if (toDoList.size() < numberPosThis || toDoList.size() == 0) {
+                        System.out.println("Номер указан неверно");
+                    } else {
+                        toDoList.remove(numberPosThis);
                     }
                 } else {
                     System.out.println("Давайте попробуем еще раз");
