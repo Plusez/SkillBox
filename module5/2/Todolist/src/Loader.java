8f7ed58df91a574b19b42d22434ed1fe4a701556
Maxim <35666350+Plusez@users.noreply.github.com>
2019-12-22 02:36:01 +0500

    module 5\2 edition 3

--- module5/2/Todolist/src/Loader.java
+++ module5/2/Todolist/src/Loader.java
@@ -16,24 +16,20 @@ public class Loader {
         for (; ; ) {
             System.out.println("Введите команду:");
             String commandName = scanner.nextLine();
-            String commandToDo = new String();
-            String numberPos = new String();
-            String nameToDo = new String();
-            Pattern mask = Pattern.compile("^([A-Z]+)\\s*(\\d+)*\\s*(\\w*)?+");
+//            String commandToDo = new String();
+//            String numberPos = new String();
+//            String nameToDo = new String();
+            Pattern mask = Pattern.compile("^([A-Z]{3,6})($|\\s*(\\d*)\\s*(.*))");                                      //^([A-Z]+)\\s*(\\d+)*\\s*(\\w*)?+");
             Matcher mat = mask.matcher(commandName);
             System.out.println(mat.groupCount());
             if (mat.find()) {
                 System.out.println("1 - " + mat.group(1));
-                System.out.println("2 - " + mat.group(2));
-                System.out.println("3 - " + mat.group(3));
+                System.out.println("2 - " + mat.group(3));
+                System.out.println("3 - " + mat.group(4));
             }
-            commandToDo = mat.group(1);
-            if (mat.group(2) != null) {
-                numberPos = mat.group(2);
-            }
-            if (mat.group(3) != null) {
-                nameToDo = mat.group(3);
-            }
+            String commandToDo = mat.group(1);
+            String numberPos = mat.group(3) != null ? mat.group(3) : "";
+            String nameToDo = mat.group(4) != null ? mat.group(4) : "";
 
             if (commandToDo.equals("LIST")) {                                                                  //LIST
                 if (toDoList.size() == 0) {
@@ -43,12 +39,19 @@ public class Loader {
                         System.out.println(i + " - " + toDoList.get(i));
                     }
                 }
+                continue;
             }
-            if (commandToDo.equals("ADD") && numberPos.isEmpty()) {                                              //ADD
+            if (commandToDo.equals("ADD") && numberPos.isEmpty() && nameToDo.isEmpty()) {                                              //ADD
                 System.out.println("Введите дело, которое необходимо добавить(ADD)");
                 nameToDo = scanner.nextLine();
                 toDoList.add(nameToDo);
+                continue;
             }
+            if (commandToDo.equals("ADD") && numberPos.isEmpty() && !nameToDo.isEmpty() ) {                    //ADD text
+                toDoList.add(nameToDo);
+                continue;
+            }
+
             if (commandToDo.equals("ADD") && !numberPos.isEmpty() && nameToDo.length() > 0) {                  //ADD n text
                 if ((toDoList.size() < Integer.parseInt(numberPos)) || toDoList.size() == 0) {
                     System.out.println("Номер строки указан неверно");
@@ -56,6 +59,7 @@ public class Loader {
                     int numberPosThis = Integer.parseInt(numberPos);
                     toDoList.add(numberPosThis, nameToDo);
                 }
+                continue;
             }
             if (commandToDo.equals("EDIT") && !numberPos.isEmpty() && nameToDo.length() > 0) {               //EDIT n text"
                 if (toDoList.size() < Integer.parseInt(numberPos) || toDoList.size() == 0) {
@@ -65,6 +69,7 @@ public class Loader {
                     toDoList.add(numberPosThis, nameToDo);
                     toDoList.remove(numberPosThis + 1);
                 }
+                continue;
             }
             if (commandToDo.equals("DELETE") && !numberPos.isEmpty()) {                                     //DELETE n
                 int numberPosThis = Integer.parseInt(numberPos);
@@ -73,6 +78,7 @@ public class Loader {
                 } else {
                     toDoList.remove(numberPosThis);
                 }
+                continue;
             } else {
                 System.out.println("Давайте попробуем еще раз");
             }
