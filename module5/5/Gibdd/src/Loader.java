8f855a66ecb2bd33cb32925c9a71ccb91ffe20a8
Maxim <35666350+Plusez@users.noreply.github.com>
2019-12-24 01:10:19 +0500

    module 5\5 edition 1

--- module5/5/Gibdd/src/Loader.java
+++ module5/5/Gibdd/src/Loader.java
@@ -22,10 +22,8 @@ public class Loader {
         ArrayList<String> baseGibdd = new ArrayList<>();
         String baseCharr = "укенхваросмт";
 
-        for (int pp5 = 1; pp5 < 100; pp5++) {
-            int nRegion = pp5;
-            for (int pp2 = 1; pp2 < 1000; pp2++) {
-                int nZnak = pp2;
+        for (int nRegion = 1; nRegion < 100; nRegion++) {
+            for (int nZnak = 1; nZnak < 1000; nZnak++) {
                 for (int pp1 = 0; pp1 < baseCharr.length(); pp1++) {
                     char firstLetter = baseCharr.charAt(pp1);
                     for (int pp3 = 0; pp3 < baseCharr.length(); pp3++) {
@@ -33,15 +31,15 @@ public class Loader {
                         for (int pp4 = 0; pp4 < baseCharr.length(); pp4++) {
                             char thirdLetter = baseCharr.charAt(pp4);
 // Проверяем соответствие номера условиям 1, 3, 4.
-                            if (nZnak < 10 || nZnak % 100 == 0 || nZnak % 111 == 0 || nZnak == nRegion) {
+                            if ((nZnak < 10 || nZnak % 100 == 0 || nZnak % 111 == 0 || nZnak == nRegion) || (firstLetter == secondLetter && firstLetter == thirdLetter)) {
                                 baseGibdd.add(String.format("%s%03d%s%s%03d", firstLetter, nZnak, secondLetter, thirdLetter, nRegion));
                             }
 //Проверяем соответствие номера условию 2.
-                            else if (firstLetter == secondLetter) {
-                                if (firstLetter == thirdLetter) {
-                                    baseGibdd.add(String.format("%s%03d%s%s%03d", firstLetter, nZnak, secondLetter, thirdLetter, nRegion));
-                                }
-                            }
+//                            else if (firstLetter == secondLetter) {
+//                                if (firstLetter == thirdLetter) {
+//                                    baseGibdd.add(String.format("%s%03d%s%s%03d", firstLetter, nZnak, secondLetter, thirdLetter, nRegion));
+//                                }
+//                            }
                         }
                     }
                 }
@@ -51,8 +49,8 @@ public class Loader {
             System.out.println(item);
         }
         Collections.sort(baseGibdd); //сортировка для бинарного поиска
-        Set hashBaseGibdd = new HashSet<String>(baseGibdd); //синхронизация для HashSet
-        SortedSet treeBaseGibdd = Collections.synchronizedSortedSet(new TreeSet<String>(baseGibdd)); //синхронизация для TreeSet
+        Set hashBaseGibdd = new HashSet<>(baseGibdd); //синхронизация для HashSet
+        Set<String> treeBaseGibdd = new TreeSet<>(baseGibdd); //синхронизация для TreeSet
 
         Scanner sc = new Scanner(System.in);
 
@@ -60,39 +58,51 @@ public class Loader {
             System.out.println("Введите номер: ");
             String inputNumber = sc.nextLine();
 // бинарный поиск
-            long time1 = System.currentTimeMillis();
+            long time1 = System.nanoTime();
             int pos = Collections.binarySearch(baseGibdd, inputNumber);
             if (pos < 0) {
                 System.out.printf("Номера %s в базе нет ", inputNumber);
-            } else System.out.printf("Номер %s в базе присутствует ", inputNumber);
-            long time2 = System.currentTimeMillis();
+            } else {
+                System.out.printf("Номер %s в базе присутствует ", inputNumber);
+            }
+            long time2 = System.nanoTime();
             Long time3 = time2 - time1;
-            System.out.println(time3 + " мс");
+            System.out.println(time3 + " нс");
 // перебор
-            long time4 = System.currentTimeMillis();
+            long time4 = System.nanoTime();
             if (baseGibdd.contains(inputNumber)) {
                 System.out.printf("Номер %s в базе присутствует ", inputNumber);
-            } else System.out.printf("Номера %s в базе нет ", inputNumber);
-            long time5 = System.currentTimeMillis();
+            } else {
+                System.out.printf("Номера %s в базе нет ", inputNumber);
+            }
+            long time5 = System.nanoTime();
             Long time6 = time5 - time4;
-            System.out.println(time6 + " мс");
+            System.out.println(time6 + " нс");
 //HashSet
-            long time7 = System.currentTimeMillis();
+            long time7 = System.nanoTime();
             if (hashBaseGibdd.contains(inputNumber)) {
                 System.out.printf("Номер %s в базе присутствует ", inputNumber);
-            } else System.out.printf("Номера %s в базе нет ", inputNumber);
-            long time8 = System.currentTimeMillis();
+            } else {
+                System.out.printf("Номера %s в базе нет ", inputNumber);
+            }
+            long time8 = System.nanoTime();
             Long time9 = time8 - time7;
-            System.out.println(time9 + " мс");
+            System.out.println(time9 + " нс");
 //TreeSet
-            long time10 = System.currentTimeMillis();
+            long time10 = System.nanoTime();
             if (treeBaseGibdd.contains(inputNumber)) {
                 System.out.printf("Номер %s в базе присутствует ", inputNumber);
-            } else System.out.printf("Номера %s в базе нет ", inputNumber);
-            long time11 = System.currentTimeMillis();
+            } else {
+                System.out.printf("Номера %s в базе нет ", inputNumber);
+            }
+            long time11 = System.nanoTime();
             Long time12 = time11 - time10;
-            System.out.println(time12 + " мс");
+            System.out.println(time12 + " нс");
 
+            System.out.format("Перебор - %d нс. Оптимален для поиска в небольших структурах, не требует преварительной подготовки структуры данных %n", time3);
+            System.out.format("BinarySearch - %d нс. Оптимален для поиска в отсортированных списках %n", time6);
+            System.out.format("TreeSet - %d нс. Оптимален для поиска в больших объемах отсортированных уникальных значений %n", time12);
+            System.out.format("HashSet - %d нс. Оптимален для поиска в больших объемах уникальных значений %n", time9);
         }
     }
 }
